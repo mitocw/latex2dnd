08eacf5cd9995711d061f74ec1e0b624688a37c9
ichuang <ichuang@mit.edu>
2017-04-16 13:53:08 -0400

    for dndspec, handle case when a label appears in multiple target boxes

--- latex2dnd/dndspec.py
+++ latex2dnd/dndspec.py
@@ -16,16 +16,24 @@ class DNDlabel(object):
     '''
     Represent a drag-and-drop draggable label.
     '''
-    def __init__(self, tex, index=None, math_exp=None, box_label=None, ltype=None):
+    def __init__(self, tex, index_set=None, index=None, math_exp=None, box_label=None, ltype=None, verbose=False):
         '''
         tex = label tex
+        index_set = dict of current index values already used, with keys=index, val=DNDlabel objects
+        index = suggested index number for this label
         math_exp = should be text formula parsable
         box_label = should be alpha only
         ltype = match or distractor
         '''
         self.tex = tex
         self.index = index
+        self.index_set = index_set
         self.ltype = ltype
+        self.verbose = verbose
+
+        if self.index in index_set:
+            raise Exception("[dndspec] Error!  non-unique index number %d for label '%s', index_set=%s" % (index, tex, index_set))
+        index_set[self.index] = self
 
         if not math_exp:
             math_exp = self.make_math_exp(tex)
@@ -36,13 +44,29 @@ class DNDlabel(object):
         self.box_label = box_label
 
         self.ddlabel = "\\DDlabel[%s]{%s}{$%s$}" % (self.math_exp, self.box_label, self.tex)
-
         self.formula_box = " ([%s]) " % self.index
+        self.ddboxes = {}	# key=index, val=ddbox string; first one uses self.index
 
-    @property
-    def ddbox(self, nwidth=1):
+    def get_new_index(self):
+        '''
+        Construct a unique new box index number, by taking max of self.index_set + 1
+        Record new index in index_set
+        '''
+        new_index = max(self.index_set.keys())+1
+        self.index_set[new_index] = self
+        return new_index
+
+    def make_ddbox(self, nwidth=1):
         bwidth = "B" * nwidth
-        return " \\DD%s{%d}{%s} " % (bwidth, self.index, self.box_label)
+        if len(self.ddboxes)==0:
+            index = self.index
+        else:
+            index = self.get_new_index()
+            if self.verbose:
+                print "[dndspec] generating new box index %d for label %s" % (index, self.tex)
+        ddbox = " \\DD%s{%d}{%s} " % (bwidth, index, self.box_label)
+        self.ddboxes[index] = ddbox
+        return ddbox
 
     def make_math_exp(self, tex):
         if all([ x in string.letters or x in string.digits for x in tex]):
@@ -117,10 +141,11 @@ class DNDspec2tex(object):
     def parse_file(self, sfn):
         self.match_labels = None
         self.distractor_labels = []
-        self.all_labels = None
+        self.all_labels = []
         self.expression = ""
         self.check_formula = None
         self.check_formula_boxes = None
+        self.comments = ""
         
         mode = None
         for k in open(sfn):
@@ -133,6 +158,11 @@ class DNDspec2tex(object):
             if k.startswith("BEGIN_EXPRESSION"):
                 mode = "in_expression"
                 continue
+            if k.startswith('%') or k.startswith('#'):
+                self.comments += "%" + k[1:]
+                continue
+            if len(k.strip())==0:	# skip empty lines
+                continue
             m = re.match('^MATCH_LABELS:(.*)', k)
             if m:
                 self.match_labels = [x.strip() for x in m.group(1).split(',')]
@@ -143,7 +173,7 @@ class DNDspec2tex(object):
                 continue
             m = re.match('^ALL_LABELS:(.*)',k)
             if m:
-                self.all_labels = [x.strip() for x in m.group(1).split(',')]
+                self.all_labels += [x.strip() for x in m.group(1).split(',')]
                 continue
             m = re.match('^CHECK_FORMULA:(.*)',k)
             if m:
@@ -160,6 +190,8 @@ class DNDspec2tex(object):
          Also generate index for labels; number the match labels first 
         since those are the boxes in the DND expression.
         '''
+        self.target_box_numbers = {}
+
         if not self.match_labels:
             raise Exception("[dndspec] Error!  No match labels specified")
         if not self.all_labels:
@@ -169,11 +201,11 @@ class DNDspec2tex(object):
         label_objs = OrderedDict()		# main dict of labels, with key=label tex, val=DNDlabel (with label_objs)
         for label in self.match_labels:
             cnt += 1
-            label_objs[label] = DNDlabel(label, index=cnt, ltype="match")
+            label_objs[label] = DNDlabel(label, index_set=self.target_box_numbers, index=cnt, verbose=self.verbose, ltype="match")
         for label in self.all_labels:
             if not label in label_objs:
                 cnt += 1
-                label_objs[label] = DNDlabel(label, index=cnt, ltype="distractor")
+                label_objs[label] = DNDlabel(label, index_set=self.target_box_numbers, index=cnt, verbose=self.verbose, ltype="distractor")
                 
         # generate \DDlabel[math_exp]{box_label}{label_tex} for each label
         # label_tex is the tex for the label, specified in LABELS
@@ -191,8 +223,24 @@ class DNDspec2tex(object):
         self.commented_expression = '%' + self.expression.replace('\n', '\n%') + '\n%'
         for label in self.match_labels:
             lobj = self.label_objs[label]
-            dnd_expression = re.sub("\s%s\s" % label, lobj.ddbox, dnd_expression)
-        self.dnd_expression = dnd_expression
+            labre = "\s%s\s" % label.replace('\\', '\\\\').replace('{', '\{').replace('^', '\^')
+            matches = re.findall(labre, dnd_expression, flags=re.M)
+            if not matches:
+                print "--> [dndspec] WARNING: no matching label '%s' found in expression!" % label
+                print "expression = %s" % dnd_expression
+                print "label regular expression = %s" % labre
+                print "search: %s" % (re.search(labre, dnd_expression, flags=re.M))
+                sys.exit(0)
+            if self.verbose:
+                print "[dndspec] found %d matches for label '%s' in expression" % (len(matches), label)
+            for match in matches:
+                # if a label appears multiple times in the expression, then
+                # the the box numbers must change, to ensure all DDB box numbers
+                # are unique
+                def make_ddbox(m):
+                    return lobj.make_ddbox(nwidth=1)
+                dnd_expression = re.sub(labre, make_ddbox, dnd_expression, count=1, flags=re.M)
+            self.dnd_expression = dnd_expression
 
     def assemble_dnd_formula(self):
         '''
@@ -239,6 +287,7 @@ class DNDspec2tex(object):
                   'COMMENTED_EXPRESSION': self.commented_expression,
                   'EXPRESSION': self.dnd_expression,
                   'DD_FORMULA': self.dd_formula,
+                  'COMMENTS': self.comments,
                   }
         for key, val in params.items():
             template = template.replace('<' + key + '>', val)
