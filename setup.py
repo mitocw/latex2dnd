c4960dcc892d57272d67d62da995e6578a4d62be
ichuang <ichuang@mit.edu>
2025-11-30 10:20:46 -0500

    replace path.py with Path in setup.py

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
             ],
         },
     install_requires=['lxml',
-                      'path.py',
+                      'Path',
                       ],
     dependency_links = [
         ],
